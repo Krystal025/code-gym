0210c9ee0e3e1104204eedfbc255e805f9b0fae5
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-09-18 15:19:12 +0900

    [level 0] Title: 카운트 업, Time: 2.79 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = {};        
+        List<Integer> list = new ArrayList<>();
+        for(int i=start_num; i<=end_num; i++){
+            list.add(i);
+        }
+        answer = list.stream().mapToInt(i -> i).toArray();
+        return answer;
+    }
+}

f8f08d6892675e2b0e3d10fd6c86a67363c06f76
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-03 10:57:48 +0900

    [level 0] Title: 0 떼기, Time: 7.82 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,14 +1,15 @@
 class Solution {
     public String solution(String n_str) {
-        int idx = 0;
-        // 0이 아닌 수가 나오는 첫번째 인덱스를 구함
-        for(int i=0; i<n_str.length(); i++){
-            if(n_str.charAt(i) != '0'){
-                idx = i;
-                break;
-            }
-        }
-        // 해당 인덱스부터 문자열을 반환 
-        return n_str.substring(idx);
-    }
+    //     int idx = 0;
+    //     for(int i=0; i<n_str.length(); i++){
+    //         if(n_str.charAt(i) != '0'){
+    //             idx = i;
+    //             break;
+    //         }
+    //     }
+    //     return n_str.substring(idx);
+    
+        // 숫자로 변환하면 자동으로 앞으 0은 제거됨 
+        return "" + Integer.parseInt(n_str); 
+    }    
 }

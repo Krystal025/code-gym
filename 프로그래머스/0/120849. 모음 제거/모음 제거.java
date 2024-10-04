7279c41d805b44ffd46b7cc1d00d960a3e4df0ab
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-04 13:52:29 +0900

    [level 0] Title: 모음 제거, Time: 0.16 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -2,15 +2,19 @@ import java.lang.StringBuilder;
 
 class Solution {
     public String solution(String my_string) {
-        StringBuilder result = new StringBuilder();
+//         StringBuilder result = new StringBuilder();
         
-        char[] charArr = my_string.toCharArray();
+//         char[] charArr = my_string.toCharArray();
         
-        for(char c : charArr){
-            if(c!='a' && c!='e' && c!='i' && c!='o' && c!='u'){
-                result.append(c);                
-            }
-        }   
-        return result.toString();
+//         for(char c : charArr){
+//             if(c!='a' && c!='e' && c!='i' && c!='o' && c!='u'){
+//                 result.append(c);                
+//             }
+//         }   
+//         return result.toString();
+        
+        String answer = my_string.replaceAll("[aeiou]", "");
+    
+        return answer;
     }
 }

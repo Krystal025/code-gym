42067d57387e026f7f18983f10d5815d9313f8e6
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-04 13:28:11 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,16 @@
+import java.lang.StringBuilder;
+
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder result = new StringBuilder();
+        
+        char[] charArr = my_string.toCharArray();
+        
+        for(char c : charArr){
+            if(c!='a' && c!='e' && c!='i' && c!='o' && c!='u'){
+                result.append(c);                
+            }
+        }   
+        return result.toString();
+    }
+}

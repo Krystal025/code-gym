414855696bb9057291f0a55cb9b6463a8671a415
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-04 15:36:43 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,14 +1,20 @@
 class Solution {
     public int solution(int n) {
+//         int answer = 0;
+        
+//         String num = String.valueOf(n);
+        
+//         for(int i=0; i<num.length(); i++){
+//             answer += Character.getNumericValue(num.charAt(i));
+//         }
+//         return answer;   
+        
         int answer = 0;
         
-        // valueOf() : 다른 타입의 값을 문자열로 변환 
-        String num = String.valueOf(n);
-        
-        // getNumericValue() : 문자의 숫자값을 반환
-        for(int i=0; i<num.length(); i++){
-            answer += Character.getNumericValue(num.charAt(i));
+        while(n>0){
+            answer += n%10;
+            n/=10;
         }
-        return answer;   
+        return answer;
     }
 }

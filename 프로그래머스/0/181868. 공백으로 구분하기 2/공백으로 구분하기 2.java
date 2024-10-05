1beb7a4c9b9c5064663f18650cd2a4cdc30ab935
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-05 11:07:00 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.50 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String my_string) {
+
+        ArrayList<String> list = new ArrayList<>();
+        
+        for(String str : my_string.split(" ")){
+            if(!str.isEmpty()){
+                list.add(str);
+            }
+        }
+           
+        return list.toArray(new String[0]);
+    }
+}

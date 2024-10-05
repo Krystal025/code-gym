ff64a8537da1ee45e474d0f452c33f9b16855111
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-05 14:47:03 +0900

    [level 0] Title: 배열 비교하기, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,21 +1,18 @@
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
         int answer = 0;
-        int sumArr1 = 0;
-        int sumArr2 = 0;
+        int sumArr1 = 0, sumArr2 = 0;
         
+        // 두 배열의 길이가 다르다면 길이로 크기 비교 
         if(arr1.length != arr2.length){
             answer = arr1.length < arr2.length ? -1 : 1;
+        // 두 배열의 길이가 같다면 요소들의 합으로 비교 
         }else{
             for(int i=0; i<arr1.length; i++){
                 sumArr1 += arr1[i];
                 sumArr2 += arr2[i];
             }
-            if(sumArr1 == sumArr2){
-                answer = 0;
-            }else{
-            answer = sumArr1 < sumArr2 ? -1 : 1;
-            }
+            answer = sumArr1 == sumArr2 ? 0 : (sumArr1 < sumArr2 ? -1 :1);
         }
         return answer;
     }

8e5cbcc0d06313aff5abc3d02651e425bfd72c8b
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-06 13:33:31 +0900

    [level 0] Title: 순서 바꾸기, Time: 12.63 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=n; i<num_list.length; i++){
+            list.add(num_list[i]);
+        }
+        for(int i=0; i<n; i++){
+            list.add(num_list[i]);
+        }
+                
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}

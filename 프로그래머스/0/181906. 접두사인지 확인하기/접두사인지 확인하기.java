58b3922854f5b1f4c125a0903e668fe4c4ae9dc3
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-07 13:27:06 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.07 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -3,12 +3,14 @@ import java.util.ArrayList;
 class Solution {
     public int solution(String my_string, String is_prefix) {
         
-        ArrayList<String> list = new ArrayList<>();
+//         ArrayList<String> list = new ArrayList<>();
                 
-        for(int i=0; i<my_string.length(); i++){
-            list.add(my_string.substring(0, i));
-        }
+//         for(int i=0; i<my_string.length(); i++){
+//             list.add(my_string.substring(0, i));
+//         }
         
-        return list.contains(is_prefix) ? 1 : 0;
+//         return list.contains(is_prefix) ? 1 : 0;
+        
+        return my_string.startsWith(is_prefix) ? 1 : 0;
     }
 }

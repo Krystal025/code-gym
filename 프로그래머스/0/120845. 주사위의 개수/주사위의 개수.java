b85fb6e4195014c34a02f1f4ba3c9f8517572c1d
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-07 11:33:31 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -3,7 +3,8 @@ class Solution {
         int answer = 1;
         
         for(int i=0; i<3; i++){
-            answer *= (int)Math.floor((double)box[i]/n);
+            // answer *= (int)Math.floor((double)box[i]/n);
+            answer *= box[i]/n;
         }
         
         return answer;

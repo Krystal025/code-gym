668792b0e5c36f778686c5c5b67cea9e81a77603
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-10 10:15:31 +0900

    [level 0] Title: 숫자 찾기, Time: 0.06 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num, int k) {
+        
+        String strNum = String.valueOf(num);
+        String strK = String.valueOf(k);
+
+        return strNum.contains(strK) ? strNum.indexOf(strK)+1 : -1;
+    }
+}

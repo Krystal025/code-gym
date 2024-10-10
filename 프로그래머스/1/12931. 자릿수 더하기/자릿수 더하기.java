4fe8da4c1b15f021e3cbff70e478daddaa83b248
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-10 22:30:32 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,13 +4,15 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        String strN = String.valueOf(n);
-        
-        for(int i=0; i<strN.length(); i++){
+        while(n != 0){
             answer += n%10;
             n /= 10;
         }
-
+//         String strN = String.valueOf(n);        
+//         for(int i=0; i<strN.length(); i++){
+//             answer += n%10;
+//             n /= 10;
+//         }
         return answer;
     }
 }

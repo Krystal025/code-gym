8009dba416d3363b672b75d9498f1eefc13e12b0
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-14 16:12:09 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.20 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,16 +1,22 @@
-import java.util.LinkedHashSet;
+import java.util.*;
 import java.lang.StringBuilder;
 
 class Solution {
     public String solution(String my_string) {
-        LinkedHashSet<Character> set = new LinkedHashSet();
-        for(char c : my_string.toCharArray()){
-            set.add(c);
-        }
-        StringBuilder str = new StringBuilder();
-        for(char c : set){
-            str.append(c);
-        }
-        return str.toString();
+        // LinkedHashSet<Character> set = new LinkedHashSet();
+        // for(char c : my_string.toCharArray()){
+        //     set.add(c);
+        // }
+        // StringBuilder str = new StringBuilder();
+        // for(char c : set){
+        //     str.append(c);
+        // }
+        // return str.toString();
+        
+        String[] strArr = my_string.split("");
+        
+        Set<String> set = new LinkedHashSet(Arrays.asList(strArr));
+        
+        return String.join("", set);
     }
 }

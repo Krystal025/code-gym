42f95bb400ba48537da9d29bf7182cb186319928
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-18 19:38:30 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.57 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,16 @@
+import java.lang.StringBuilder;
+
+class Solution {
+    public String solution(int n) {
+        StringBuilder str = new StringBuilder();
+        
+        for(int i=1; i<=n; i++){
+            if(i%2 != 0){
+                str.append("수");
+            }else{
+                str.append("박");
+            }
+        }
+        return str.toString();
+    }
+}

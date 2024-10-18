0b62010ad94451a54a4fa9e05cf20edaa618a4b3
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-18 12:01:29 +0900

    [level 1] Title: 내적, Time: 0.04 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int total = 0;
+        
+        for(int i=0; i<a.length; i++){
+            total += (a[i] * b[i]);
+        }
+        return total;
+    }
+}

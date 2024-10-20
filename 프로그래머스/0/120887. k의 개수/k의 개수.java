a7261163cb410820a9e03d74e30832900059896b
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-20 12:35:08 +0900

    [level 0] Title: k의 개수, Time: 3.09 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -3,16 +3,27 @@ import java.lang.StringBuilder;
 class Solution {
     public int solution(int i, int j, int k) {
         
-        StringBuilder str = new StringBuilder();
+//         StringBuilder str = new StringBuilder();
+//         int count = 0;
+        
+//         for(int num=i; num<=j; num++){
+//             str.append(num);
+//         }
+//         char[] charArr = str.toString().toCharArray();
+        
+//         for(char c : charArr){
+//             if(Character.getNumericValue(c) == k) count++;
+//         }
+//         return count;
         int count = 0;
         
         for(int num=i; num<=j; num++){
-            str.append(num);
-        }
-        char[] charArr = str.toString().toCharArray();
-        
-        for(char c : charArr){
-            if(Character.getNumericValue(c) == k) count++;
+            // num을 직접 나눌 경우 for문에서 증가시키는 num에 영향을 미침 
+            int tmp = num; 
+            while(tmp > 0){
+                if(tmp%10 == k) count++;
+                tmp /= 10;
+            }
         }
         return count;
     }

baa50d94f1a0ecc39fc587a9311865685c3a8fb2
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-10-21 12:29:08 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length/n][n];
+        
+        for(int i=0; i<num_list.length/n; i++){
+            answer[i] = Arrays.copyOfRange(num_list, i*n, (i+1)*n);       
+        }
+        return answer;
+    }
+}

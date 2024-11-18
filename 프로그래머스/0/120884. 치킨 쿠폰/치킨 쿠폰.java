8f5ecc65b1864e1d03c23673c11f8fb407af9c2a
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-11-18 09:29:58 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -2,16 +2,11 @@ class Solution {
     public int solution(int chicken) {
         int service = 0, coupon = 0;
 
-        while(chicken >= 10){
-            service += chicken/10;
-            coupon += chicken%10;
-            chicken /= 10;
-        }
-        while(chicken+coupon >= 10){
+        while(chicken+coupon >= 10){ 
             int totalCoupons = chicken+coupon;
-            service += totalCoupons/10;
-            coupon = totalCoupons%10;
-            chicken = totalCoupons/10;
+            service += totalCoupons / 10;
+            coupon = totalCoupons % 10;
+            chicken = totalCoupons / 10;
         }
                 
         return service;

9de3df6fbdd4325608feb13ef6a0e70d6630c832
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-11-23 12:15:23 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.65 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -1,19 +1,20 @@
-import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.ArrayList;
 import java.util.Collections;
 
 class Solution {
     public String solution(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        TreeMap<Character, Integer> map = new TreeMap<>();
         ArrayList<String> list = new ArrayList<>();
         for(char c : s.toCharArray()){
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        for(HashMap.Entry<Character, Integer> entry : map.entrySet()){
+        for(Map.Entry<Character, Integer> entry : map.entrySet()){
             if(entry.getValue() == 1) 
                 list.add(Character.toString(entry.getKey()));
         }
-        Collections.sort(list);
+        // Collections.sort(list);
         return String.join("", list);
     }
 }

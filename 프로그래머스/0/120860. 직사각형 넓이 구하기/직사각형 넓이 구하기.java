08e75d1cc9b44c830888fdfee8ae8198ef77a8fa
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-12-01 20:55:24 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.06 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
@@ -3,8 +3,12 @@ class Solution {
         int x = 0;
         int y = 0;
         for(int i=0; i<4; i++){
-            x = dots[0][0] != dots[1][0] ? Math.abs(dots[0][0]-dots[1][0]) : Math.abs(dots[0][0]-dots[2][0]);
-            y = dots[0][1] != dots[1][1] ? Math.abs(dots[0][1]-dots[1][1]) : Math.abs(dots[0][1]-dots[2][1]);
+            x = dots[0][0] != dots[1][0] 
+                ? Math.abs(dots[0][0]-dots[1][0]) 
+                : Math.abs(dots[0][0]-dots[2][0]);
+            y = dots[0][1] != dots[1][1] 
+                ? Math.abs(dots[0][1]-dots[1][1]) 
+                : Math.abs(dots[0][1]-dots[2][1]);
         }
         return x * y;
     }

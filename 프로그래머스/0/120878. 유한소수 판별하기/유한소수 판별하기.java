a2e0f48fa28fa2056d5e3c48620d3b16bdd81b4f
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2024-12-20 19:13:54 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.09 ms, Memory: 89.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b) {
+        int gcd = 0;
+        // 두 수의 최대공약수를 구함
+        for(int i=1; i<=Math.min(a, b); i++){
+            if(a%i == 0 && b%i == 0) gcd = i;
+        }
+        // 1보다 큰 최대공약수가 있다면 나눠서 기약분수로 만듦 
+        b /= gcd;
+        while(b%2 == 0 || b%5 == 0){
+            if(b%2 == 0) b /= 2;
+            if(b%5 == 0) b /= 5;
+        }        
+        return b==1 || b==2 || b==5 ? 1 : 2;
+        }
+}

b53e3c032e16781b876b22fa016043ecfcd0cbc6
Krystal.K <126314784+Krystal025@users.noreply.github.com>
2025-01-13 16:43:52 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.20 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -6,14 +6,9 @@ class Solution{
 
         Arrays.sort(A);
         Arrays.sort(B);
-        
-        for(int i=0; i<B.length/2; i++){
-            int temp = B[i];
-            B[i] = B[B.length-1-i];
-            B[B.length-1-i] = temp;
-        }
+
         for(int i=0; i<A.length; i++){
-            sum += A[i]*B[i];
+            sum += A[i] * B[B.length-1-i];
         }
         
         return sum;

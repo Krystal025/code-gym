af01f51560af8ad14303d102fcd6f70433675405
SuJeong.K <126314784+Krystal025@users.noreply.github.com>
2026-08-03 20:29:52 +0900

    [level 2] Title: 가장 큰 수, Time: 107.93 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -9,16 +9,10 @@ class Solution {
         }
         
         Arrays.sort(arr, (a, b) -> (b+a).compareTo(a+b));
-        if(arr[0].startsWith("0")) return "0";
         
-//         StringBuilder sb = new StringBuilder();
-        
-//         for(String str : arr){
-//             sb.append(str);
-//         }
-//         return sb.toString();
-        
-        // StringBuilder 사용 또는 join() 사용
-        return String.join("", arr); 
+        String answer = String.join("", arr); 
+    
+        if(answer.startsWith("0")) return "0";
+        return answer;
     }
 }
